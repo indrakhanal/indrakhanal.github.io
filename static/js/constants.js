bf033e3116b85f581b09c59f411547c2eb21b8a7
Indra Khanal <37183477+indrakhanal@users.noreply.github.com>
2021-12-02 21:55:49 +0545

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_greet";
-const rasa_server_url = "https://blc-chatbot.herokuapp.com//webhooks/rest/webhook";
+const rasa_server_url = "http://indra-bot.herokuapp.com/webhooks/rest/webhook";
 const sender_id = uuidv4();
